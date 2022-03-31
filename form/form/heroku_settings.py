4701e5e4888197d4b44770ac337c3b8d224dd490
Ivuska <fischerova.ivka@gmail.com>
2022-04-01 00:25:16 +0200

    Set CSRF_TRUSTED_ORIGINS.

--- form/form/heroku_settings.py
+++ form/form/heroku_settings.py
@@ -4,6 +4,8 @@ import dj_database_url
 
 ALLOWED_HOSTS = ['frontendmentor-intro-component.herokuapp.com']
 
+CSRF_TRUSTED_ORIGINS = ['https://frontendmentor-intro-component.herokuapp.com']
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
