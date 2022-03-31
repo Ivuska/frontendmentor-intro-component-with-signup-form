f85c65e15221a768de7a0d8e9ec60c1b525e60a2
Ivuska <fischerova.ivka@gmail.com>
2022-04-01 00:33:17 +0200

    Increase the limit of password field.

--- form/fromapp/models.py
+++ form/fromapp/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
   name = models.CharField(max_length=100)
   surname = models.CharField(max_length=100)
   email_address = models.EmailField(max_length=254, unique=True)
-  password = models.CharField(max_length=50)
+  password = models.CharField(max_length=254)
 
   def save(self, **kwargs):
     self.password = make_password(self.password)
